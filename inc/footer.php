59322f7c65c57c86213c60be8b5e385acb1b26b5
ridvankuntug <ridvankuntug@gmail.com>
2020-03-05 23:21:41 +0300

    Kalıp Dosyaların Yüklenmesi

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center" style="margin-bottom:0; margin-top:30px;"><!--Sayfa alt bölmesi ayarları-->
+<h2>Online Ön Tanı</h2>
+<h4>Sağlığınız bizim için önemli.</h4>
+</div>
+
+</body>
+</html>
+<!--Bütün sayfaların altında çağırılacak-->
