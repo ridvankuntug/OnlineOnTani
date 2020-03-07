edb7a540e559a45325566bf67901e99c353035f3
ridvankuntug <ridvankuntug@gmail.com>
2020-03-07 16:05:06 +0300

    rutin

--- funcs/headerphp.php
+++ funcs/headerphp.php
@@ -1,5 +1,6 @@
 <?php
   error_reporting(0);//Basit uyarıları gizliyoruz
+  header('Content-type: text/html; charset=utf-8');
   require("funcs/baglanti.php");//baglanti.php sayfasını çağırıyoruz
   session_start();//Session global değişkenini başlatıyoruz
 ?>
